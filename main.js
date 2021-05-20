6e4785c605eca625bbcd9d4dd07c90cef5b0986b
jb-larriviere <76693792+jb-larriviere@users.noreply.github.com>
2021-05-20 16:03:27 +0200

    Add main functions to the program

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
 //MIXED MESSAGES
+
+const zodiacSigns = ['Aries', 'Taurus', 'Gemini', 'Cancer', 'Leo', 'Virgo', 'Libra', 'Scorpius', 'Sagittarius', 'Capricornus', 'Aquarius', 'Pisces'];
+const whatYoullBe = ['happy', 'loved', 'healthy', 'creative', 'optimistic', 'peacefull', 'adventurous', 'arrogant', 'depressive', 'impatient', 'jealous', 'unhappy'];
+const howLong = ['days', 'weeks', 'month', 'year', 'decade', 'century'];
+
+const pickRandom = array => array[Math.floor(Math.random()*array.length)];
+
+const createRandomHoroscope = () => {
+    let sign = pickRandom(zodiacSigns);
+    let adjective = pickRandom(whatYoullBe);
+    let duration = pickRandom(howLong);
+
+    return `${sign} : due to the influence of the moon, you shoul'd feel quite ${adjective} over the next ${duration}.`;
+}
+
+const logRandomHoroscope = () => console.log(createRandomHoroscope());
+
+logRandomHoroscope();
